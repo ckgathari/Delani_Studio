3797ac2b39056ff59aa04b4247480d3442acdeee
ckgathari <Chege>
2023-04-23 01:15:43 +0300

    added hover effect on portfolio section

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+$("#portfolio1").hover(function() {
+    $(this).css('cursor','pointer').attr('title', 'DARK PAGE PROJECT');
+}, function() {
+    $(this).css('cursor','auto');
+});
+
+$("#portfolio2").hover(function() {
+    $(this).css('cursor','pointer').attr('title', 'ONTARIO PAGE PROJECT');
+}, function() {
+    $(this).css('cursor','auto');
+});
+
+$("#portfolio3").hover(function() {
+    $(this).css('cursor','pointer').attr('title', 'BROWN IMAGE PROJECT');
+}, function() {
+    $(this).css('cursor','auto');
+});
+
+$("#portfolio4").hover(function() {
+    $(this).css('cursor','pointer').attr('title', 'JIM CARREY PROJECT');
+}, function() {
+    $(this).css('cursor','auto');
+});
+
+$("#portfolio5").hover(function() {
+    $(this).css('cursor','pointer').attr('title', 'HAVE A GOOD DAY PAGE');
+}, function() {
+    $(this).css('cursor','auto');
+});
+
+$("#portfolio6").hover(function() {
+    $(this).css('cursor','pointer').attr('title', 'FORM ENTRY PROJECT');
+}, function() {
+    $(this).css('cursor','auto');
+});
+
+$("#portfolio7").hover(function() {
+    $(this).css('cursor','pointer').attr('title', 'BURNED PAGE PROJECT');
+}, function() {
+    $(this).css('cursor','auto');
+});
+
+$("#portfolio8").hover(function() {
+    $(this).css('cursor','pointer').attr('title', 'BAILEY WONGER PROJECT');
+}, function() {
+    $(this).css('cursor','auto');
+});
