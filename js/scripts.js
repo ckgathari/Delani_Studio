8ccddc3389d272075aafaec0a673cc3bd3b35d1b
ckgathari <Chege>
2023-04-23 22:43:58 +0300

    Added hover effect on portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
+$(function(){
+
+    $("#col-md-4-1").click(function(){
+        $(".hidden1").slideToggle(1600) 
+        $(".img1").slideToggle(1600)       
+    });
+
+    $("#col-md-4-2").click(function(){
+        $(".hidden2").slideToggle(1600)
+        $(".img2").slideToggle(1600)
+    });
+    
+    $("#col-md-4-3").click(function(){
+        $(".hidden3").slideToggle(1600)
+        $(".img3").slideToggle(1600)
+    });
+});
+
+
+
 $("#portfolio1").hover(function() {
     $(this).css('cursor','pointer').attr('title', 'DARK PAGE PROJECT');
 }, function() {
